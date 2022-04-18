a412f24879c2c81916de18d09fd151ecbdd7ce0b
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2022-04-19 08:07:43 +0900

    [백준] 11052번 카드 구매하기

--- src/_11052_카드구매하기.java
+++ src/_11052_카드구매하기.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class _11052_카드구매하기 {
+import java.util.Scanner;
+
+public class _11052_카드구매하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[1000];
+
+        for(int i = 0; i < n; i++){
+            dp[i] = sc.nextInt();
+            for(int j = 0; j < (i % 2 == 0 ? i / 2 : i / 2 + 1); j++){
+                int dpTmp = dp[j] + dp[i - j - 1];
+                if(dp[i] < dpTmp) dp[i] = dpTmp;
+            }
+        }
+        System.out.println(dp[n - 1]);
+    }
 }
